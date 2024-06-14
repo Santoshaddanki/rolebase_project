64c5b5258d134f4d657a80e62b5b918063971fe4
BALASARVAN12 <91976828+BALASARVAN12@users.noreply.github.com>
2024-06-14 23:25:11 +0530

    Created Login Page

---
+++ frontend/src/Login.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+function Login() {
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header">
+              <h3 className="text-center">Login</h3>
+            </div>
+            <div className="card-body">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    id="email"
+                    placeholder="Enter Email"
+                  /> <br/>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    id="password"
+                    placeholder="Enter Password"
+                  />
+                </div> <br/>
+                <button type="submit" className="btn btn-primary btn-block">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
